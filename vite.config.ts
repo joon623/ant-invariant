db1a87ca066db833595d46be0bda7f3b7b88d7f4
jun <nnamjun94@gmail.com>
2024-12-02 23:27:48 +0900

    Feat: added type definition file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vitest/config";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: { entry: resolve(__dirname, "src/invariant.ts"), formats: ["es"] },
+    lib: {
+      entry: resolve(__dirname, "src/invariant.ts"),
+      fileName: "ant-invariant",
+      formats: ["es"],
+    },
     rollupOptions: {
       output: {
         exports: "named",
@@ -21,4 +26,11 @@ export default defineConfig({
   esbuild: {
     keepNames: true,
   },
+  plugins: [
+    dts({
+      entryRoot: "src",
+      include: ["src/**/*.ts"],
+      outDir: "dist",
+    }),
+  ],
 });
